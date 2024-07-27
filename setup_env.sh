b7b6e104476db699ec479276adcf926f940f63f0
acathe <wade.03taverna@icloud.com>
2024-07-28 01:18:45 +0800

    feat: use remote env

--- setup_env.sh
+++ setup_env.sh
@@ -93,17 +93,17 @@ install_zsh() {
 }
 
 install_ohmyzsh() {
-    # Install Oh-My-Zsh from the official.
-    # Use RUNZSH=no to skip running Zsh and prevent interrupting the script.
-    # Ref. https://ohmyz.sh/#install
-    RUNZSH=no \
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
     if [ -n "$ENABLE_CHINA_MIRROR" ]; then
-        # Switch the existing Oh-My-Zsh to Tsinghua mirror.
         # Ref. https://mirrors.tuna.tsinghua.edu.cn/help/ohmyzsh.git/#%E5%88%87%E6%8D%A2%E5%B7%B2%E6%9C%89%20ohmyzsh%20%E8%87%B3%E9%95%9C%E5%83%8F%E6%BA%90
-        git -C "$ZSH" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh.git
-        git -C "$ZSH" pull
+        RUNZSH=no \
+            REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/ohmyzsh.git \
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    else
+        # Install Oh-My-Zsh from the official.
+        # Use RUNZSH=no to skip running Zsh and prevent interrupting the script.
+        # Ref. https://ohmyz.sh/#install
+        RUNZSH=no \
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
 
     # Sync the previous ~/.zshrc with Oh-My-Zsh.
