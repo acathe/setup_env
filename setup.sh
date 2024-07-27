acef8849bff6fb47d20c69b86cfec75b4b287637
acathe <wade.03taverna@icloud.com>
2024-07-28 06:26:44 +0800

    fix: branch

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 #
 # Respects the following environment variables:
 #   ENABLE_CHINA_MIRROR -
+#   _BRANCH              -
 #
 
 set -e
@@ -11,15 +12,15 @@ main() {
         export ENABLE_CHINA_MIRROR=1
     fi
 
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/terminal/apt.sh")"
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/terminal/zsh.sh")"
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/terminal/omz.sh")"
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/terminal/omz_plugins.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/terminal/apt.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/terminal/zsh.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/terminal/omz.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/terminal/omz_plugins.sh")"
 
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/lang/cpp.sh")"
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/lang/golang.sh")"
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/lang/rust.sh")"
-    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/master/lang/python.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/lang/cpp.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/lang/golang.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/lang/rust.sh")"
+    bash -c "$(curl -fsSL "https://raw.githubusercontent.com/acathe/setup_env/orbstack_machines/${_BRANCH}/lang/python.sh")"
 
     unset ENABLE_CHINA_MIRROR
 }
