fd92f8b2666afe1f59abad35c2bbede0d1a772f6
Acathe <49369640+acathe@users.noreply.github.com>
2025-11-01 23:37:17 +0800

    feat: container main.sh add run container

--- container/main.sh
+++ container/main.sh
@@ -2,6 +2,30 @@
 
 set -euo pipefail
 
+USER="${USER:-}"
+
+parse_args() {
+    POSITIONAL=()
+    while (($# > 0)); do
+        case "$1" in
+            --user)
+                numOfArgs=1 # number of switch arguments
+                if (($# < numOfArgs + 1)); then
+                    shift $#
+                else
+                    USER="$2"
+                    POSITIONAL+=("$1" "$2")
+                    shift $((numOfArgs + 1)) # shift 'numOfArgs + 1' to bypass switch and its value
+                fi
+                ;;
+            *) # unknown flag/switch
+                POSITIONAL+=("$1")
+                shift
+                ;;
+        esac
+    done
+}
+
 main() {
     [[ -f "./base/build.sh" ]] \
         && bash "./base/build.sh" "$@"
@@ -11,9 +35,21 @@ main() {
 
     [[ -f "./dev/build.sh" ]] \
         && bash "./dev/build.sh" "$@"
+
+    docker build . \
+        -t dev-container/main
+
+    [[ ! -d "$HOME/Projects" ]] && mkdir -p "$HOME/Projects"
+
+    docker run -d \
+        --name dev-container \
+        -v "$HOME/Projects:/home/$USER/Projects" \
+        dev-container/main
 }
 
 if [[ $0 == "${BASH_SOURCE[0]}" ]]; then
     cd "$(dirname "${BASH_SOURCE[0]}")"
+    parse_args "$@"
+    set -- "${POSITIONAL[@]}" # restore positional params
     main "$@"
 fi
