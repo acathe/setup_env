41f537e3ea79b0396c46d3b5b2940c3cbe754aaa
Acathe <49369640+acathe@users.noreply.github.com>
2025-11-01 22:57:09 +0800

    feat: add main setup script with support for macOS and Debian

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 BRANCH="${BRANCH:-master}"
+SETUP="${SETUP:-debian}"
 
 parse_args() {
     POSITIONAL=()
@@ -17,6 +18,15 @@ parse_args() {
                     shift $((numOfArgs + 1)) # shift 'numOfArgs + 1' to bypass switch and its value
                 fi
                 ;;
+            --setup)
+                numOfArgs=1 # number of switch arguments
+                if (($# < numOfArgs + 1)); then
+                    shift $#
+                else
+                    SETUP="$2"
+                    shift $((numOfArgs + 1)) # shift 'numOfArgs + 1' to bypass switch and its value
+                fi
+                ;;
             *) # unknown flag/switch
                 POSITIONAL+=("$1")
                 shift
@@ -25,11 +35,32 @@ parse_args() {
     done
 }
 
-main() {
+macos() {
+    if [[ ! -d "/Library/Developer/CommandLineTools" ]]; then
+        xcode-select --install
+    fi
+}
+
+debian() {
     if [[ -z "$(command -v git)" ]]; then
         sudo apt-get update
         sudo apt-get install -y git
     fi
+}
+
+main() {
+    case "$SETUP" in
+        macos)
+            macos
+            ;;
+        debian | container)
+            debian
+            ;;
+        *)
+            echo "Unsupported setup: $SETUP" >&2
+            return 1
+            ;;
+    esac
 
     tmpdir="$(mktemp -du "/tmp/setup_env.XXXXXX")"
 
@@ -38,7 +69,7 @@ main() {
         --single-branch \
         --branch "$BRANCH"
 
-    bash "$tmpdir/container/main.sh" "$@"
+    bash "$tmpdir/$SETUP/main.sh" "$@"
 }
 
 parse_args "$@"
