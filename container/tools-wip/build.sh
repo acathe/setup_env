fcb945e5b32d657849c7514e95e16ca61f201bb0
Acathe <49369640+acathe@users.noreply.github.com>
2025-11-02 05:23:25 +0800

    feat: add tools

---
+++ container/tools-wip/build.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TOOLS_PROTOBUF="${TOOLS_PROTOBUF:-false}"
+TOOLS_THRIFT="${TOOLS_THRIFT:-false}"
+
+parse_args() {
+    POSITIONAL=()
+    while (($# > 0)); do
+        case "$1" in
+            --tools-protobuf)
+                TOOLS_PROTOBUF=true
+                shift # shift once since flags have no values
+                ;;
+            --tools-thrift)
+                TOOLS_THRIFT=true
+                shift # shift once since flags have no values
+                ;;
+            *) # unknown flag/switch
+                POSITIONAL+=("${1}")
+                shift
+                ;;
+        esac
+    done
+}
+
+main() {
+    local from="dev-container/terminal:latest"
+
+    if $TOOLS_PROTOBUF; then
+        docker build . \
+            -t dev-container/tools/protobuf \
+            -f ./protobuf/Dockerfile \
+            --build-arg "from=$from"
+
+        from="dev-container/tools/protobuf:latest"
+    fi
+
+    if $TOOLS_THRIFT; then
+        docker build . \
+            -t dev-container/tools/thrift \
+            -f ./thrift/Dockerfile \
+            --build-arg "from=$from"
+
+        from="dev-container/tools/thrift:latest"
+    fi
+
+    docker build . \
+        -t dev-container/tools \
+        --build-arg "from=$from"
+}
+
+if [[ $0 == "${BASH_SOURCE[0]}" ]]; then
+    cd "$(dirname "${BASH_SOURCE[0]}")"
+    parse_args "$@"
+    set -- "${POSITIONAL[@]}" # restore positional params
+    main "$@"
+fi
