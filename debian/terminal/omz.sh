f2bc633b3732d73b23aa20ac17f60fb697310bd3
Acathe <49369640+acathe@users.noreply.github.com>
2025-11-01 16:37:55 +0800

    fix: append debian plugins

--- debian/terminal/omz.sh
+++ debian/terminal/omz.sh
@@ -32,7 +32,7 @@ install_plugin() {
         return 1
     fi
 
-    sed -i "s/^plugins=(.*)/plugins=(git z sudo)/" "$HOME/.zshrc"
+    sed -i "/^plugins=(/s/)/ z sudo vscode)/" "$HOME/.zshrc"
 
     # Ref. https://github.com/Pilaton/OhMyZsh-full-autoupdate?tab=readme-ov-file#installing
     git clone "https://github.com/Pilaton/OhMyZsh-full-autoupdate.git" "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate"
